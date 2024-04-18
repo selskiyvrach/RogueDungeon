9bcf129a645d5af4dd2bc92b2f459912af98cb89
ivan kriuchkov <i.kriuchkov@corp.nekki.ru>
2024-04-18 23:40:39 +0400

    action 1st frame callback fix, sample surroundings for better spatial info

--- Assets/RogueDungeon/Scripts/Actions/Action.cs
+++ Assets/RogueDungeon/Scripts/Actions/Action.cs
@@ -36,6 +36,7 @@ namespace RogueDungeon.Actions
             _isRewinding = false;
             CurrentFrame = 1;
             OnStarted();
+            TryRaiseCallback();
         }
 
         public void Tick()
