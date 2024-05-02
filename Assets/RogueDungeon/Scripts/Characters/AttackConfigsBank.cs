ea006337c9ff6f83dc4ee3e9102932abfd6620ec
ivan kriuchkov <i.kriuchkov@corp.nekki.ru>
2024-05-02 10:53:38 +0400

    added attack configs bank

---
+++ Assets/RogueDungeon/Scripts/Characters/AttackConfigsBank.cs
@@ -0,0 +1,12 @@
+﻿using RogueDungeon.Data;
+using RogueDungeon.Items;
+using UnityEngine;
+
+namespace RogueDungeon.Characters
+{
+    [CreateAssetMenu(menuName = "Configs/Characters/AttackConfigsBank", fileName = "AttackConfigsBank", order = 0)]
+    public class AttackConfigsBank : ScriptableDictionary<AttackConfig>
+    {
+        
+    }
+}
--- Assets/RogueDungeon/Scripts/Characters/EnemyCharacterConfig.cs
+++ Assets/RogueDungeon/Scripts/Characters/EnemyCharacterConfig.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using RogueDungeon.Actions;
 using RogueDungeon.Items;
 using UnityEngine;
@@ -10,31 +9,24 @@ namespace RogueDungeon.Characters
     [CreateAssetMenu(menuName = "Configs/Characters/EnemyCharacter", fileName = "EnemyCharacter", order = 0)]
     public class EnemyCharacterConfig : CharacterConfig
     {
-        [Serializable]
-        private class AttackConfigById
-        {
-            public string Id;
-            public AttackConfig Attack;
-        }
-        [SerializeField] private AttackConfigById[] _attackConfigs;
-        
         [field: SerializeField] public EnemyCharacterConfig Extends { get; private set; }
-        [field: SerializeField] public AttackPattern[] AttackPatters { get; private set; }
+        [field: SerializeField] public AttackPattern[] AttackPatterns { get; private set; }
+        [field: SerializeField] public AttackConfigsBank AttackConfigsBank { get; private set; }
 
         public AttackConfig GetAttackConfig(string id)
         {
-            var attack = _attackConfigs.FirstOrDefault(n => n.Id == id)?.Attack;
-            if(attack == null && Extends != null)
-                attack = Extends.GetAttackConfig(id);
-            Assert.IsNotNull(attack, $"Attack config with id '{id}' is missing on character config with id'{Id}'");
-            return attack;
+            var attackConfig = AttackConfigsBank.TryGetValue(id, out var attack)
+                ? attack
+                : Extends.GetAttackConfig(id);
+            Assert.IsNotNull(attackConfig, $"Attack config with id '{id}' is missing on character config with id'{Id}'");
+            return attackConfig;
         }
 
         public override CharacterController CreateController(Character character, ActionFactory actionFactory)
         {
             var patterns = Extends == null 
-                ? AttackPatters 
-                : Extends.AttackPatters.Concat(AttackPatters);
+                ? AttackPatterns 
+                : Extends.AttackPatterns.Concat(AttackPatterns);
             
             return new AiCharacterController(character, actionFactory, patterns);
         }
