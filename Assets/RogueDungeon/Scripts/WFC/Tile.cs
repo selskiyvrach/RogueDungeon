2bc2a6bab772288ffaa07e0c32efb8f3742c4137
ivan kriuchkov <i.kriuchkov@corp.nekki.ru>
2024-05-12 23:12:08 +0400

    wfc in progress

---
+++ Assets/RogueDungeon/Scripts/WFC/Tile.cs
@@ -0,0 +1,15 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace RogueDungeon.WFC
+{
+    [CreateAssetMenu(menuName = "Configs/WFC/Tile", fileName = "Tile", order = 0)]
+    public class Tile : ScriptableObject
+    {
+        [field: PreviewField, SerializeField] public Sprite Sprite { get; private set; }
+        [field:SerializeField] public bool Up {get; private set;}
+        [field:SerializeField] public bool Right {get; private set;}
+        [field:SerializeField] public bool Down {get; private set;}
+        [field:SerializeField] public bool Left {get; private set;}
+    }
+}
---
+++ Assets/RogueDungeon/Scripts/WFC/WFC.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace RogueDungeon.WFC
+{
+    public class Cell
+    {
+        public int X { get; }
+        public int Y { get; }
+        public bool Collapsed => Options.Count == 1;
+        public List<Tile> Options { get; }
+
+        public Cell(int x, int y, IEnumerable<Tile> options)
+        {
+            X = x;
+            Y = y;
+            Options = new List<Tile>(options);
+        }
+    }
+
+    public class WFC
+    {
+        private readonly int _sizeX = 10;
+        private readonly int _sizeY = 10;
+        
+        private readonly List<Cell> _cells = new();
+        
+        private Tile[] _tiles;
+        private Cell _currentCell;
+        
+        private int _iterations;
+
+        public void Run()
+        {
+            InitCells();
+        }
+
+        private void InitCells()
+        {
+            for (var x = 0; x < _sizeX; x++)
+            for (var y = 0; y < _sizeY; y++)
+                _cells.Add(new Cell(x, y, _tiles));
+        }
+    }
+}
