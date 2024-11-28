48d72e08c23ae462d3a1e5cbd7a07eaa9547ccb8
selskiyvrach <selskiyvrach@gmail.com>
2024-11-29 01:51:52 +0400

    fully functioning installer generator

---
+++ Assets/RogueDungeon/Scripts/Common/InstallerGenerator/CreateFactoryInstallerAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class CreateFactoryInstallerAttribute : Attribute
+{
+    public Type BindAs { get; }
+    public Type[] SerializedParams { get; }
+    public Type[] FactoryParams { get; }
+
+    public CreateFactoryInstallerAttribute(Type bindAs = null, Type[] serializedParams = null, Type[] factoryParams = null)
+    {
+        BindAs = bindAs;
+        SerializedParams = serializedParams;
+        FactoryParams = factoryParams;
+    }
+}
--- Assets/RogueDungeon/Scripts/Common/InstallerGenerator/InstallerGenerator.cs
+++ Assets/RogueDungeon/Scripts/Common/InstallerGenerator/InstallerGenerator.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 public static class InstallerGenerator
 {
-    private const string InstallersFolder = "Assets/GeneratedInstallers";
+    private const string InstallersFolder = "Assets/Installers";
 
     [MenuItem("Generate/Installer Classes")]
     public static void GenerateInstallerClasses()
@@ -20,12 +20,12 @@ public static class InstallerGenerator
 
         var typesWithAttribute = AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(assembly => assembly.GetTypes())
-            .Where(type => type.GetCustomAttribute<CreateScriptableInstallerAttribute>() != null);
+            .Where(type => type.GetCustomAttribute<CreateFactoryInstallerAttribute>() != null);
 
         foreach (var type in typesWithAttribute)
         {
-            var attribute = type.GetCustomAttribute<CreateScriptableInstallerAttribute>();
-            var fileName = attribute.Name ?? type.Name + "GeneratedInstaller.cs";
+            var attribute = type.GetCustomAttribute<CreateFactoryInstallerAttribute>();
+            var fileName = type.Name + "FactoryInstaller.cs";
             var scriptPath = Path.Combine(InstallersFolder, fileName);
             var directory = Path.GetDirectoryName(scriptPath);
 
@@ -36,7 +36,7 @@ public static class InstallerGenerator
 
             if (!File.Exists(scriptPath))
             {
-                var installerCode = GenerateInstallerCode(type, attribute.BindAs ?? type, attribute.SerializedFields ?? Array.Empty<Type>());
+                var installerCode = GenerateInstallerCode(type, attribute.BindAs ?? type, attribute.SerializedParams ?? Array.Empty<Type>(), attribute.FactoryParams ?? Array.Empty<Type>());
                 File.WriteAllText(scriptPath, installerCode);
                 Debug.Log($"Generated installer script for {type.Name} at {scriptPath}");
             }
@@ -55,12 +55,11 @@ public static class InstallerGenerator
     {
         var typesWithAttribute = AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(assembly => assembly.GetTypes())
-            .Where(type => type.GetCustomAttribute<CreateScriptableInstallerAttribute>() != null);
+            .Where(type => type.GetCustomAttribute<CreateFactoryInstallerAttribute>() != null);
 
         foreach (var type in typesWithAttribute)
         {
-            var attribute = type.GetCustomAttribute<CreateScriptableInstallerAttribute>();
-            var fileName = attribute.Name ?? type.Name + "GeneratedInstaller.asset";
+            var fileName = type.Name + "FactoryInstaller.asset";
             var assetPath = Path.Combine(InstallersFolder, fileName);
             var assetFullPath = Path.Combine(Application.dataPath, InstallersFolder["Assets/".Length..], fileName);
 
@@ -78,7 +77,8 @@ public static class InstallerGenerator
         AssetDatabase.Refresh();
         AssetDatabase.SaveAssets();
     }
-private static string GenerateInstallerCode(Type targetType, Type bindAs, Type[] serializeFields)
+
+    private static string GenerateInstallerCode(Type targetType, Type bindAs, Type[] serializeFields, Type[] factoryParameters)
 {
     // Collect all namespaces for the using statements
     var usingStatements = string.Join("\n", new List<string>
@@ -88,11 +88,13 @@ private static string GenerateInstallerCode(Type targetType, Type bindAs, Type[]
     }
     .Concat(new[] { targetType.Namespace, bindAs.Namespace })
     .Concat(serializeFields.Select(n => n.Namespace))
+    .Concat(factoryParameters.Select(n => n.Namespace))
     .Where(n => !string.IsNullOrEmpty(n)).Distinct()
     .Select(n => $"using {n};"));
 
     var fieldNames = new Dictionary<string, int>();
-    var serializedFields = string.Join("", serializeFields.Select(n =>
+    var finalFieldNames = new List<string>();
+    var serializedFields = string.Join("\n\t", serializeFields.Select(n =>
     {
         var baseFieldName = char.ToLower(n.Name[0]) + n.Name.Substring(1);
 
@@ -100,22 +102,31 @@ private static string GenerateInstallerCode(Type targetType, Type bindAs, Type[]
         {
             fieldNames[baseFieldName]++;
         }
-
-        var finalFieldName = $"{baseFieldName}{(fieldNames[baseFieldName] > 1 ? fieldNames[baseFieldName] : "")}";
+        var finalFieldName = $"_{baseFieldName}{(fieldNames[baseFieldName] > 1 ? fieldNames[baseFieldName] : "")}";
+        finalFieldNames.Add(finalFieldName);
         
-        return $"\n    [SerializeField] private {n.Name} _{finalFieldName};";
+        return $"[SerializeField] private {n.Name} {finalFieldName};";
     }));
 
+    var bindings = string.Join("\n\t\t", Enumerable.Range(0, serializeFields.Length).Select(n => $"subContainer.BindInterfacesAndSelfTo<{serializeFields[n].Name}>().FromInstance({finalFieldNames[n]}).AsSingle();"));
+    var parameters = factoryParameters.Length == 0 ? "" : string.Join(", ", factoryParameters.Select(n => n.Name)) + ", ";
+    var parametersBindings = string.Join("\n\t\t",Enumerable.Range(0, factoryParameters.Length).Select(n => $"subContainer.BindInterfacesAndSelfTo<{factoryParameters[n].Name}>().FromInstance(param{n}).AsSingle();"));
+    
     return 
 $@"{usingStatements}
 
-public class {targetType.Name}Installer : ScriptableObject
+public class {targetType.Name}FactoryInstaller : ScriptableObjectInstaller<{targetType.Name}FactoryInstaller>, IFactory<{parameters}{bindAs.Name}>
 {{
     {serializedFields}
 
-    public {bindAs.Name} Install(DiContainer container)
+    public override void InstallBindings() => 
+        Container.Bind<IFactory<{parameters}{bindAs.Name}>>().To<{targetType.Name}FactoryInstaller>().FromInstance(this);
+
+    public {bindAs.Name} Create({string.Join(", ", Enumerable.Range(0, factoryParameters.Length).Select(n => $"{factoryParameters[n].Name} param{n}"))})
     {{
-        var subContainer = container.CreateSubContainer();
+        var subContainer = Container.CreateSubContainer();
+        {parametersBindings}
+        {bindings}
         subContainer.Bind<{bindAs.Name}>().To<{targetType.Name}>().FromNew().AsSingle();
         return subContainer.Resolve<{bindAs.Name}>(); 
     }}
