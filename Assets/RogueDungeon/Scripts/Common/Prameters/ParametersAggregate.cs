f9cb5e18ebcb6350bea99134525587c50891320d
selskiyvrach <selskiyvrach@gmail.com>
2024-12-03 19:34:29 +0400

    parameters aggregate

---
+++ Assets/RogueDungeon/Scripts/Common/Prameters/ParametersAggregate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common.Prameters
+{
+    public class ParametersAggregate
+    {
+        private readonly Dictionary<object, object> _parametersMap = new();
+        
+        public Parameter Get<T>(T id) where T : struct, Enum => 
+            _parametersMap.TryGetValue(id, out var parameters) ? ((Parameters<T>)parameters).Get(id) : null;
+
+        public void Add<T>(T id, Parameter parameter) where T : struct, Enum
+        {
+            if(!_parametersMap.ContainsKey(id))
+                _parametersMap.Add(id, new Parameters<T>());
+            ((Parameters<T>)_parametersMap[id]).Add(id, parameter);   
+        }
+    }
+}
