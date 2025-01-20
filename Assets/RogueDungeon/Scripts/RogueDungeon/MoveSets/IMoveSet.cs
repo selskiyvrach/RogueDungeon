2ae1bb2f6d269cb2fd2f36bfdb1217c6936ebe4a
selskiyvrach <selskiyvrach@gmail.com>
2025-01-20 22:44:48 +0400

    move set system

--- Assets/RogueDungeon/Scripts/RogueDungeon/MoveSets/IMoveSet.cs
+++ Assets/RogueDungeon/Scripts/RogueDungeon/MoveSets/IMoveSet.cs
@@ -1,86 +1,170 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Common.Animations;
+using Common.Behaviours;
 using Common.Fsm;
 using RogueDungeon.Fsm;
+using UniRx;
 
 namespace RogueDungeon.MoveSets
 {
-    public interface IMoveSet
+    internal class MoveSetBehaviour : StateMachineBehaviour
     {
-        public IReadOnlyList<IMove> Elements { get; }
+        public MoveSetBehaviour(IStatesFactory statesFactory, ILogger logger = null) : base(statesFactory, logger)
+        {
+        }
+
+        protected override void ToStartState() => 
+            To<MoveSetIdleState>();
     }
 
-    public class MoveExecutionState : BoundToAnimationState
+    public interface ITryTransitionToMoveGetter
     {
-        private readonly IMove _move;
-        private readonly IMoveInputReader _moveInputReader;
-        protected override AnimationData Animation => new (_move.AnimationName, _move.Duration);
+        bool TryTransitionTo(MoveId id);
+    }
 
-        public MoveExecutionState(IMove move, IAnimator animator) : base(animator) => 
-            _move = move;
+    internal interface IMoveSetMovesGetter
+    {
+        public IMove Get(MoveId id);
+        public IEnumerable<IMove> All { get; }
+    }
+    
+    internal class MoveSetInternalFacade : IBehaviourInternalFacade, ICurrentMoveIdGetter, ICurrentMoveIdSetter, IPendingMoveIdGetter, IPendingMoveIdSetter
+    {
+        private MoveId? _currentMoveId;
+        private MoveId? _pendingMoveId;
+
+        MoveId? ICurrentMoveIdGetter.CurrentMoveId => _currentMoveId;
+        MoveId? ICurrentMoveIdSetter.CurrentMoveId
+        {
+            set => _currentMoveId = value;
+        }
+
+        MoveId? IPendingMoveIdGetter.PendingMoveId => _pendingMoveId;
+        MoveId? IPendingMoveIdSetter.PendingMoveId
+        {
+            set => _pendingMoveId = value;
+        }
+    }
+
+    internal interface IMove
+    {
+        MoveId Id { get; }
+        string AnimationName {get;}
+        float Duration { get; }
+        IMove[] Transitions { get; }
+    }
+
+    public readonly struct MoveId
+    {
+        
+    }
+
+    public readonly struct Event
+    {
+        public string Name { get; }
+
+        public Event(string name) => 
+            Name = name;
+    }
+
+    public interface IMoveSetEventObservable
+    {
+        ISubject<Event> OnEvent { get; }
+    }
+
+    internal interface IMoveSetEventRaiser
+    {
+        void Raise(Event @event);
+    }
+
+    internal interface ICurrentMoveIdGetter
+    {
+        MoveId? CurrentMoveId { get; }
+    }
+
+    internal interface ICurrentMoveIdSetter
+    {
+        MoveId? CurrentMoveId { set; }
+    }
+    
+    internal interface IPendingMoveIdGetter
+    {
+        MoveId? PendingMoveId { get; }
+    }
+
+    internal interface IPendingMoveIdSetter
+    {
+        MoveId? PendingMoveId { set; }
+    }
+    
+    internal class MoveSetIdleState : IState
+    {
+        private readonly IMoveSetMovesGetter _moveSetMovesGetter;
+        private readonly IPendingMoveIdSetter _pendingMoveIdSetter;
+        private readonly ITryTransitionToMoveGetter _tryTransitionToMoveGetter;
+        
+        public void CheckTransitions(IStateChanger stateChanger)
+        {
+            var moveToTransitionTo = _moveSetMovesGetter.All.FirstOrDefault(n => _tryTransitionToMoveGetter.TryTransitionTo(n.Id));
+            if(moveToTransitionTo is null)
+                return;
+            
+            _pendingMoveIdSetter.PendingMoveId = moveToTransitionTo.Id;
+            stateChanger.To<MoveExecutionState>();
+        }
+    }
+
+    internal class MoveExecutionState : BoundToAnimationState
+    {
+        private readonly IMoveSetMovesGetter _moveSetMovesGetter;
+        private readonly IPendingMoveIdGetter _pendingMoveIdGetter;
+        private readonly IPendingMoveIdSetter _pendingMoveIdSetter;
+        private readonly ICurrentMoveIdGetter _currentMoveIdGetter;
+        private readonly ICurrentMoveIdSetter _currentMoveIdSetter;
+        private readonly ITryTransitionToMoveGetter _tryTransitionToMoveGetter;
+        private readonly IMoveSetEventRaiser _eventRaiser;
+
+        private IMove _move;
+
+        protected override AnimationData Animation => new (_move.AnimationName, _move.Duration);
+        
+        public MoveExecutionState(IAnimator animator, IMoveSetMovesGetter moveSetMovesGetter, IPendingMoveIdGetter pendingMoveIdGetter, IPendingMoveIdSetter pendingMoveIdSetter, ICurrentMoveIdSetter currentMoveIdSetter, ICurrentMoveIdGetter currentMoveIdGetter, ITryTransitionToMoveGetter tryTransitionToMoveGetter, IMoveSetEventRaiser eventRaiser) : base(animator)
+        {
+            _moveSetMovesGetter = moveSetMovesGetter;
+            _pendingMoveIdGetter = pendingMoveIdGetter;
+            _pendingMoveIdSetter = pendingMoveIdSetter;
+            _currentMoveIdSetter = currentMoveIdSetter;
+            _currentMoveIdGetter = currentMoveIdGetter;
+            _tryTransitionToMoveGetter = tryTransitionToMoveGetter;
+            _eventRaiser = eventRaiser;
+        }
+
+        public override void Enter()
+        {
+            _currentMoveIdSetter.CurrentMoveId = _pendingMoveIdGetter.PendingMoveId;
+            _pendingMoveIdSetter.PendingMoveId = null;
+            _move = _moveSetMovesGetter.Get((MoveId)_currentMoveIdGetter.CurrentMoveId);
+            base.Enter();
+        }
+
+        protected override void OnEvent(string name) => 
+            _eventRaiser.Raise(new Event(name));
 
         public override void CheckTransitions(IStateChanger stateChanger)
         {
             if(!IsFinished)
                 return;
 
-            // i created a type-based state machine
-            // but here it requires other way of identification for states
-            
-            for (var i = 0; i < _move.Transitions.Length; i++)
+            var transitionTo = _move.Transitions.FirstOrDefault(n => _tryTransitionToMoveGetter.TryTransitionTo(n.Id));
+            if (transitionTo == null)
             {
-                var move = _move.Transitions[i];
-                if (move is not IInputDependableMove requiresInput)
-                {
-                }
+                stateChanger.To<MoveSetIdleState>();
+                return;
             }
+
+            _pendingMoveIdSetter.PendingMoveId = transitionTo.Id;
+            stateChanger.To<MoveExecutionState>();
         }
-    }
-
-    public interface IMove
-    {
-        // can have expected animation callbacks
-        // checks if they and only they are being risen
-        // has handlers for the callbacks
-        
-        string AnimationName {get;}
-        float Duration { get; }
-        IMove[] Transitions { get; }
-    }
-
-    [AttributeUsage(AttributeTargets.Class)]
-    public class EditorNameAttribute : Attribute
-    {
-        public string Name { get; }
-
-        public EditorNameAttribute(string name) => 
-            Name = name;
-    }
-
-    public interface IInputDependableMove
-    {
-        bool HasRequiredInput(IMoveInputReader inputReader);
-        void ConsumeInput(IMoveInputReader inputReader);
-    }
-
-    public interface IMoveInputReader
-    {
-        bool TryConsume<T>() where T : IMoveInputDefinition;
-    }
-
-    public interface IMoveInputDefinition
-    {
-        
-    }
-
-    public interface IEnterPointMove
-    {
-        
-    }
-
-    public interface IExitPointMove
-    {
-        
     }
 }
