8a49708a460014ae936d2c36eecd0750255c8ab5
selskiyvrach <selskiyvrach@gmail.com>
2025-03-06 23:11:08 +0400

    new animation base class

--- Assets/RogueDungeon/Scripts/Common/Animations/AnimationPlayer.cs
+++ Assets/RogueDungeon/Scripts/Common/Animations/AnimationPlayer.cs
@@ -1,81 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using Common.UtilsDotNet;
+using System.Linq;
+using Common.Time;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Common.Animations
 {
-    public sealed class AnimationPlayer : MonoBehaviour, IAnimator
+    public interface IAnimation
     {
-        [SerializeField] private Animation _referenceToAnimation;
-        [SerializeField] private GameObject _animatedObject;
-        
-        private readonly Queue<(float time, string eventName)> _eventsToPlay = new();
-        
-        private AnimationClip _clip;
-        private float _duration;
-        private bool _isLooped;
-        private float _playTime;
-        private float _timeScale;
-        private IDisposable _updateSubscriber;
+        bool IsFinished { get; }
+        event Action<string> OnEvent;
+        void Play();
+        void Stop();
+    }
+    
+    // create regular animation wrapper and spritesheet animation
 
+    public abstract class Animation : IAnimation
+    {
+        private readonly Ticker _ticker = new();
+        private float _timePassed;
+        
+        protected abstract float Duration { get; }
+        protected abstract IEnumerable<(float time, string name)> Events { get; }
+        
+        public bool IsFinished => _timePassed >= Duration;
         public event Action<string> OnEvent;
-        public bool IsFinished => _playTime >= _clip.length;
 
-        public void Play(AnimationData animationData)
+        public virtual void Play()
         {
-            _duration = animationData.Duration;
-            _isLooped = false;
-            _clip = animationData.Clip;
-            _timeScale = _clip.length / _duration;
-            _playTime = 0;
-            PrepareEvents();
-            UpdatePlayback();
+            _timePassed = 0;
+            ApplyAnimation(0);
+            _ticker.Start(Tick);
         }
 
-        private void Update()
+        protected virtual void Tick(float timeDelta)
         {
-            if(_clip == null)
-                return;
+            Assert.IsFalse(IsFinished);
+            var lastFrameTime = _timePassed;
+            _timePassed += timeDelta;
+            ApplyAnimation(Mathf.Clamp01(_timePassed / Duration));
             
-            _playTime += Time.deltaTime * _timeScale;
-            EvaluateEvents();
-            UpdatePlayback();
-        }
-
-        private void UpdatePlayback()
-        {
-            _clip.SampleAnimation(_animatedObject, _playTime);
+            foreach (var (time, name) in Events ?? Enumerable.Empty<(float, string)>())
+            {
+                Assert.IsTrue(time >= 0 && time < Duration);
+                if(time > lastFrameTime && time < _timePassed)
+                    OnEvent?.Invoke(name);
+            }
             
-            if (!IsFinished)
-                return;
-
-            if (_isLooped)
-            {
-                _playTime = 0;
-                PrepareEvents();
-            }
-            else
-                _clip = null;
+            if(IsFinished)
+                Stop();
         }
 
-        private void EvaluateEvents()
-        {
-            while (_eventsToPlay.TryPeek(out var peekedEvent) && peekedEvent.time < _playTime) 
-                OnEvent?.Invoke(_eventsToPlay.Dequeue().eventName);
-        }
+        public virtual void Stop() => 
+            _ticker.Stop();
 
-        private void PrepareEvents()
-        {
-            _eventsToPlay.Clear();
-            for (var i = 0; i < _clip.events.Length; i++)
-            {
-                var @event = _clip.events[i];
-                if (@event.stringParameter.IsNullOrEmpty())
-                    Debug.LogError($"Invalid event name at index {i} in animation clip {_clip.name}");
-                else
-                    _eventsToPlay.Enqueue((@event.time, @event.stringParameter));
-            }
-        }
+        protected abstract void ApplyAnimation(float timeNormalized);
     }
 }
