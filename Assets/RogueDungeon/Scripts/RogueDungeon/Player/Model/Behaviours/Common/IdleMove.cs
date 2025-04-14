2efb5b46cd327c69a38b1ff5a09dfa966df45165
selskiyvrach <selskiyvrach@gmail.com>
2025-04-15 03:47:59 +0400

    added player walk and idle animations

--- Assets/RogueDungeon/Scripts/RogueDungeon/Player/Model/Behaviours/Common/IdleMove.cs
+++ Assets/RogueDungeon/Scripts/RogueDungeon/Player/Model/Behaviours/Common/IdleMove.cs
@@ -5,7 +5,8 @@ namespace RogueDungeon.Player.Model.Behaviours.Common
     public class IdleMove : PlayerMove
     {
         private readonly Player _player;
-        protected override float Duration => _player.Config.IdleAnimationDuration; 
+        protected override float Duration => _player.Config.IdleAnimationDuration;
+        protected override bool IsLooping => true;
 
         public IdleMove(Player player, IAnimation animation, string id) : base(id, animation) => 
             _player = player;
